ee4109eb5de62420f9643b96a1553108a6be5498
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-05-12 17:04:50 +0200

    aggiunta HomePage con messaggio di benvenuto

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <div>
+            <h1> Benvenuto nel nostro store!</h1>
+            <p>Scopri i nostri prodotti</p>
+        </div>
+    );
+}
+
+export default HomePage;
