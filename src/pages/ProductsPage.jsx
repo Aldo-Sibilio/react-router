9bc8cce3f397da1c114611d6c92a3b7ea3e34011
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-05-13 16:05:08 +0200

    aggiunto Link ai prodotti per navigare alla pagina di dettaglio

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router";
 
 function ProductsPage() {
 
-const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -21,16 +22,20 @@ const [products, setProducts] = useState([]);
       <h1>Prodotti</h1>
       <div>
         {products.map((product) => (
-          <div key={product.id}>
-            <img src={product.image} alt={product.title} />
-            <h3>{product.title}</h3>
-            <p>{product.price} €</p>
-          </div>
+          // Link che porta alla pagina di dettaglio con l'id del prodotto
+          <Link to={`/products/${product.id}`} key={product.id}>
+            <div>
+              <img src={product.image} alt={product.title} />
+              <h3>{product.title}</h3>
+              <p>{product.price} €</p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
   );
 }
+
 
 
 export default ProductsPage;
